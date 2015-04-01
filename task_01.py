2b928e715d668574ba75ed2b493233ccb070a9a2
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-04-01 03:01:48 +0000

    added task_01 and task_02

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+# *-* coding: utf-8 *-*
+"""Creating a dictionary."""
+
+GRADE_DATA = {
+    'Luke': {'subject':['math', 'etiquette', 'grammar', 'gym']},
+    'Han': {'subject': ['math', 'etiquette', 'grammar', 'gym']},
+    'C-3PO': {'subject': ['math', 'etiquette', 'grammar', 'gym']}
+}
---
+++ task_02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Different ways to access a dictionary."""
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BANDS_NAMES = data.BANDS.keys()
+
+
+
+
+
+

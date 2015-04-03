387a285537b32f7d4b66aa62ef3977d3eb99872f
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-04-03 22:36:16 +0000

    updated task_06.py

--- task_06.py
+++ task_06.py
@@ -7,4 +7,5 @@ import data
 
 SUPER_SIDEKICKS = {}
 for HERO, HERO_DATA in data.SUPERHEROES.iteritems():
-    SUPER_SIDEKICKS[HERO] = # Complete this line
+    SUPER_SIDEKICKS[HERO] = HERO_DATA.get('pet')
+    print SUPER_SIDEKICKS
